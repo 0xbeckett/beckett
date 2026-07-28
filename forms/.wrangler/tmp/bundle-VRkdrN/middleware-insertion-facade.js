cc597e0957888d1c859bae936b3c71c232127f44
Beckett <0xbeckett@users.noreply.github.com>
2026-07-27 21:43:49 -0700

    beckett: #62 checkpoint (wk_aa0b59c1)

---
+++ forms/.wrangler/tmp/bundle-VRkdrN/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/home/beckett/Projects/beckett/.beckett/worktrees/#62/forms/src/index.js";
+				import * as __MIDDLEWARE_0__ from "/home/beckett/.bun/install/global/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/home/beckett/.bun/install/global/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/home/beckett/Projects/beckett/.beckett/worktrees/#62/forms/src/index.js";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
